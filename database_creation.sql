9d538542aa6f1b22dad3a5153600ec83982a725c
Jack Runge <jrunge96@gmail.com>
2024-06-06 20:01:03 -0600

    fixed typo in ipynb, added sql file for ingestion into db

---
+++ database_creation.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE tiktok.db;
+CREATE TABLE music_data(
+    track_name varchar(200) NOT NULL,
+    artist_name varchar(200) NOT NULL,
+    artist_pop int NOT NULL,
+    album varchar(200) NOT NULL,
+    track_pop int NOT NULL,
+    danceability numeric NOT NULL,
+    energy numeric NOT NULL,
+    loudness numeric NOT NULL,
+    key int NOT NULL,
+    speechiness  numeric NOT NULL,
+    acousticness numeric NOT NULL,
+    instrumentalness numeric NOT NULL,
+    liveness numeric NOT NULL,
+    valence numeric NOT NULL,
+    tempo numeric NOT NULL,
+    year int NOT NULL);
