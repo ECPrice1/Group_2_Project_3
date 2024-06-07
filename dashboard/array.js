95f8a8b043dd9f59f633d0174a0b77a39e0c37ac
ECPrice1 <ericprice@Erics-MacBook-Pro.local>
2024-06-07 13:49:39 -0600

    added functionality to dropdown and slight update to .html

--- dashboard/array.js
+++ dashboard/array.js
@@ -2,19 +2,19 @@ let years = [2019, 2020, 2021, 2022];
 
 // Metric variable for function inputs
 let metric = "danceability";
-// let metric = "rental_rate";
-// let metric = "replacement_cost";
+// let metric = "energy";
+// let metric = "tempo";
 
-// Function to calculate and plot the average of a metric by rating
+// Function to calculate and plot the average of a metric by year
 function plotMetric(tiktokData, years, metric) {
   // Initialize an array to hold metric averages
   let metricArray = [];
 
-  // Loop through the array of ratings
+  // Loop through the array of years
   for (let i =0; i < years.length; i++) {
     // Store the year at index `i` for evaluation
     year = years[i];
-    console.log(year)
+    
     // Initialize variables to increment
     let count = 0;
     let total = 0;
@@ -23,11 +23,11 @@ function plotMetric(tiktokData, years, metric) {
     for (let j = 0; j < tiktokData.length; j++) {
       // Store the song at index `j` for evaluation
       row = tiktokData[j];
-      console.log(row)
+      
 
-      // Compare `rating` value to `rating` provided as argument
+      // Compare `Year` value to `year` provided as argument
       if (row.Year == year){
-        console.log(metricArray)
+        
         // Increment by `metric` argument value
         total += row[metric];
         // Increment by one
@@ -41,8 +41,8 @@ function plotMetric(tiktokData, years, metric) {
     metricArray.push(meanValue);
   }
   
-  // Assign `ratings` array argument to `x`
-  // Assign the `metricArray` with averages for each rating to `y`
+  // Assign `years` array argument to `x`
+  // Assign the `metricArray` with averages for each year to `y`
   let trace1 = {
     x: years,
     y: metricArray,
@@ -68,3 +68,16 @@ function plotMetric(tiktokData, years, metric) {
 
 // Invoke the plotting function
 plotMetric(tiktokData, years, metric);
+
+function updatePlot() {
+    // Get the selected metric from the dropdown
+    let selectedMetric = document.getElementById("metricDropdown").value;
+  
+    // Replot the graph with the selected metric
+    plotMetric(tiktokData, years, selectedMetric);
+  }
+  
+  // Call the updatePlot function when the page loads to initially plot the default metric
+  document.addEventListener("DOMContentLoaded", function() {
+    updatePlot();
+  });
