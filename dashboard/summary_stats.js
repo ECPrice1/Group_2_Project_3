eb4f36012d590fc57ea03ca5fcf540ab0b1dc9ab
skylorfriday17 <skylorfriday@MacBook-Pro.local>
2024-06-12 00:30:44 -0600

    Included some comments to explain my code

--- dashboard/summary_stats.js
+++ dashboard/summary_stats.js
@@ -1,11 +1,16 @@
+// Assign initial metric to be displayed in the panel.
 let firstMetric = 'danceability';
 
+// Create a function to gather metric data
 function summaryStats(tiktokData, metric) {
     
+    // Create empty array to store metric data
     let metricData = [];
 
+    // Set a total to add to for calulations.
     let total = 0
 
+    // Loop through data to gather specific metric data.
     for (i = 0; i < tiktokData.length; i++) {
 
         let object = tiktokData[i];
@@ -15,6 +20,7 @@ function summaryStats(tiktokData, metric) {
         total += object[metric];
     }
 
+    // Perform statistical calculations on metric data.
     let metricMin = Math.min(...metricData);
     let metricMax = Math.max(...metricData);
     let metricMean = total / metricData.length;
@@ -28,10 +34,13 @@ function summaryStats(tiktokData, metric) {
     let metricQ2 = sortedArr[Math.floor(metricData.length * 0.50)];
     let metricQ3 = sortedArr[Math.floor(metricData.length * 0.75)];
 
+    // Grab the summary panel with d3.
     let panel = d3.select("#summary_stat");
 
+    // Clear any calculations that might have been there before.
     panel.html("");
 
+    // Add new calculations to the stat panel.
     panel.append("p").text(`Min: ${metricMin}`);
     panel.append("p").text(`Max: ${metricMax}`);
     panel.append("p").text(`Mean: ${metricMean}`);
@@ -41,9 +50,10 @@ function summaryStats(tiktokData, metric) {
     panel.append("p").text(`Q3: ${metricQ3}`);
 };
 
+// Run the function with given metric.
 summaryStats(tiktokData, firstMetric);
 
-
+// Create function to run on new selection to update panel and histogram.
 function updateChartSummary() {
 
     // Assign variable to extract metric from selection to display in panel.
@@ -52,5 +62,6 @@ function updateChartSummary() {
     // Recall the summary function.
     summaryStats(tiktokData, selection);
 
+    // Recall the function to update the histogram.
     updateHist();
 };
