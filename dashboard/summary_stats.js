603b749b3011ae0082c97b176f7a27c3575e2b4e
Jack Runge <jrunge96@gmail.com>
2024-06-12 18:09:01 -0600

    rounded to 3 digits

--- dashboard/summary_stats.js
+++ dashboard/summary_stats.js
@@ -24,10 +24,11 @@ function summaryStats(tiktokData, metric) {
     let metricMin = Math.min(...metricData);
     let metricMax = Math.max(...metricData);
     let metricMean = total / metricData.length;
+    let roundedMean = metricMean.toFixed(3)
     
     let deviations = metricData.map(num => Math.pow(num - metricMean, 2)); 
     let stdDeviation = Math.sqrt(deviations.reduce((acc,val) => acc + val, 0) / metricData.length);
-
+    let rounded_std_Deviation = stdDeviation.toFixed(3)
     let sortedArr = metricData.slice().sort((a,b) => a - b)
 
     let metricQ1 = sortedArr[Math.floor(metricData.length * 0.25)];
@@ -43,8 +44,8 @@ function summaryStats(tiktokData, metric) {
     // Add new calculations to the stat panel.
     panel.append("p").text(`Min: ${metricMin}`);
     panel.append("p").text(`Max: ${metricMax}`);
-    panel.append("p").text(`Mean: ${metricMean}`);
-    panel.append("p").text(`Std. Dev: ${stdDeviation}`);
+    panel.append("p").text(`Mean: ${roundedMean}`);
+    panel.append("p").text(`Std. Dev: ${rounded_std_Deviation}`);
     panel.append("p").text(`Q1: ${metricQ1}`);
     panel.append("p").text(`Q2: ${metricQ2}`);
     panel.append("p").text(`Q3: ${metricQ3}`);
